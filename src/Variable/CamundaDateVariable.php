61b4fcf64a9a960ae5fa45982f1bec7c4fe144b2
Madhu Mohan <madhu.mohan@partscheck.com.au>
2022-02-15 14:25:42 +1000

    Adding Camunda Date Variable

---
+++ src/Variable/CamundaDateVariable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace StrehleDe\CamundaClient\Variable;
+use Cake\Chronos\Chronos;
+
+/**
+ * Class CamundaDateVariable
+ * @package StrehleDe\CamundaClient\Variable
+ */
+class CamundaDateVariable extends CamundaVariable
+{
+    /**
+     * CamundaBooleanVariable constructor.
+     * @param bool $value
+     */
+    public function __construct($value = '')
+    {
+        parent::__construct(CamundaVariable::TYPE_DATE, $value);
+    }
+
+
+    /**
+     * @param mixed $value
+     * @return CamundaVariable
+     */
+    public function setValue($value): CamundaVariable
+    {
+        return parent::setValue($value);
+    }
+}
